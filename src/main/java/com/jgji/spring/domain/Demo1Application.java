2f7ec733098cb98af06978501351f317bf254c98
junggu <elementbyid@gmail.com>
2020-01-29 19:13:51 +0900

    type : Style
    패키지명 변경

--- src/main/java/com/jgji/spring/domain/Demo1Application.java
+++ src/main/java/com/jgji/spring/domain/Demo1Application.java
@@ -1,4 +1,4 @@
-package com.jgji.spring.domain.word;
+package com.jgji.spring.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
