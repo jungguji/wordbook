acd8ed432185278beedd5de65e0c414d39aaaeaf
junggu <elementbyid@gmail.com>
2020-01-30 15:00:26 +0900

    Type: Feat
    회원가입시 Password BCryprt로 Encoding 하도록 수정

--- src/main/java/com/jgji/spring/domain/config/SecurityConfig.java
+++ src/main/java/com/jgji/spring/domain/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.jgji.spring.domain.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -30,5 +32,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .logout()
                 .permitAll();
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
