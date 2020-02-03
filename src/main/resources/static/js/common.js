279061edeed595f890f8122dfa513ede97f30e13
junggu <elementbyid@gmail.com>
2020-02-03 17:27:08 +0900

    type : Feat
    ajax user token check common으로 이동

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -13,3 +13,10 @@ function clearliTagClass() {
 function addClassliTag(index) {
 	document.querySelectorAll("li")[index].setAttribute("class", "active");
 }
+
+const token = $("meta[name='_csrf']").attr("content");
+const header = $("meta[name='_csrf_header']").attr("content");
+
+$(document).ajaxSend(function(e, xhr, options) {
+    xhr.setRequestHeader(header, token);
+});
