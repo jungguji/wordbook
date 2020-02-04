75e362c14ba9c711112538eef99e3db4d7d7c982
junggu <elementbyid@gmail.com>
2020-02-04 17:19:42 +0900

    type : Refactor
    불 필요한 변수 제거

--- src/main/java/com/jgji/spring/domain/word/model/Row.java
+++ src/main/java/com/jgji/spring/domain/word/model/Row.java
@@ -3,7 +3,6 @@ package com.jgji.spring.domain.word.model;
 public class Row {
 
     private String text = null;
-    private Integer cell = null;
     
     public Row() {
         super();
@@ -16,14 +15,4 @@ public class Row {
     public void setText(String text) {
         this.text = text;
     }
-
-    public Integer getCell() {
-        return cell;
-    }
-
-    public void setCell(Integer cell) {
-        this.cell = cell;
-    }
-    
-    
 }
