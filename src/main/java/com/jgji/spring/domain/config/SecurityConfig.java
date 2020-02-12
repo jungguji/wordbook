33944d0d5bb0c026dbcba9d7c5b59bfa37fdb85a
junggu <elementbyid@gmail.com>
2020-02-12 15:43:36 +0900

    type : Feat
    login page 수정

--- src/main/java/com/jgji/spring/domain/config/SecurityConfig.java
+++ src/main/java/com/jgji/spring/domain/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**").permitAll()
                 .and()
             .formLogin()
+                .loginPage("/login")
                 .permitAll()
                 .and()
             .logout()
--- src/main/java/com/jgji/spring/domain/user/controller/UserController.java
+++ src/main/java/com/jgji/spring/domain/user/controller/UserController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,12 @@ public class UserController {
     
     @Autowired
     private WordService wordService;
+    
+    @GetMapping("/login")
+    public String  initLoginForm(Model model) {
+        System.out.println("@@@@@@@@@@@@@@@@");
+        return "thymeleaf/login";
+    }
     
     @GetMapping("/user/create")
     public String initCreationForm(Map<String, Object> model) {
