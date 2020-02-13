bda323181dd957bb4cd6422622ec37c1cde68a06
junggu <elementbyid@gmail.com>
2020-02-13 18:56:20 +0900

    type : Refactor
    공통으로 사용 되는 코드 common으로 이동

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -20,3 +20,12 @@ const header = $("meta[name='_csrf_header']").attr("content");
 $(document).ajaxSend(function(e, xhr, options) {
     xhr.setRequestHeader(header, token);
 });
+
+const empty = (value) => { 
+    if (value === null) return true 
+    if (typeof value === 'undefined') return true 
+    if (typeof value === 'string' && value === '') return true 
+    if (Array.isArray(value) && value.length < 1) return true 
+    if (typeof value === 'object' && value.constructor.name === 'Object' && Object.keys(value).length < 1 && Object.getOwnPropertyNames(value) < 1) return true 
+    if (typeof value === 'object' && value.constructor.name === 'String' && Object.keys(value).length < 1) return true // new String() return false }
+};
