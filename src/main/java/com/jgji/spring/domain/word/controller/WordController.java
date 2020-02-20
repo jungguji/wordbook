33bbfc03abb37d8057945732e291e62b319494ca
junggu <elementbyid@gmail.com>
2020-02-20 18:25:59 +0900

    type : Feat
    Word service Mybatis로 변경ing

--- src/main/java/com/jgji/spring/domain/word/controller/WordController.java
+++ src/main/java/com/jgji/spring/domain/word/controller/WordController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
