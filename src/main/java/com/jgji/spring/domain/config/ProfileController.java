db2b3e38d26856bcec7812b14a0ffd89e4b52ece
junggu <junggu.ji.dev@gmail.com>
2020-06-03 17:08:15 +0900

    type : Fix

--- src/main/java/com/jgji/spring/domain/config/ProfileController.java
+++ src/main/java/com/jgji/spring/domain/config/ProfileController.java
@@ -4,9 +4,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
 public class ProfileController {
     private final Environment env;
@@ -17,14 +14,16 @@ public class ProfileController {
 
     @GetMapping("/profile")
     public String getProfile() {
-        List<String> profiles = Arrays.asList(env.getActiveProfiles());
-        List<String> prodProfiles = Arrays.asList("prod", "prod1", "prod2");
-        String defaultProfile = profiles.isEmpty() ? "default" : prodProfiles.get(0);
+//        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+//        List<String> prodProfiles = Arrays.asList("prod", "prod1", "prod2");
+//        String defaultProfile = profiles.isEmpty() ? "default" : prodProfiles.get(0);
+//
+//        System.out.println(" @@@@ getProfile() @@@@ ");
+//        return profiles.stream()
+//                .filter(prodProfiles::contains)
+//                .findAny()
+//                .orElse(defaultProfile);
 
-        System.out.println(" @@@@ getProfile() @@@@ ");
-        return profiles.stream()
-                .filter(prodProfiles::contains)
-                .findAny()
-                .orElse(defaultProfile);
+        return "test";
     }
 }
