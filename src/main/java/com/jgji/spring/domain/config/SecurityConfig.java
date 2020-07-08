5d08182763b16ac433c8a6f5bbf6b0721c2294bf
junggu <junggu.ji.dev@gmail.com>
2020-07-08 21:06:52 +0900

    type : Bug
    한글 깨지는 것 처리

--- src/main/java/com/jgji/spring/domain/config/SecurityConfig.java
+++ src/main/java/com/jgji/spring/domain/config/SecurityConfig.java
@@ -7,7 +7,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,6 +21,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter, CsrfFilter.class);
+
         http.authorizeRequests()
                 .antMatchers("/word/**").authenticated()
                 .antMatchers("/user/profile").authenticated()
