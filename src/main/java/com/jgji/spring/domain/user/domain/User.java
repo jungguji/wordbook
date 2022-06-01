f679b4d21b2d34ad1274de87f8271b2979984426
jungguji <junggu.ji.dev@gmail.com>
2022-06-01 18:01:42 +0900

    refactor : user 리팩토링 중 ..

--- src/main/java/com/jgji/spring/domain/user/domain/User.java
+++ src/main/java/com/jgji/spring/domain/user/domain/User.java
@@ -35,8 +35,30 @@ public class User {
         this.password = password;
     }
 
-    public void update(String password) {
+    public void changePassword(String password) {
         this.password = password;
+    }
+
+    public String changeRandomPassword() {
+        this.password = getRandomPassword();
+        return this.password;
+    }
+
+    private String getRandomPassword() {
+        char passwordArray[] = new char[] {
+                '0','1','2','3','4','5','6','7','8','9',
+                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                '!','@','#','$','%','^','&','*','(',')'};
+
+        StringBuilder ranPw = new StringBuilder();
+
+        for (int i = 0; i < 16; i++) {
+            int selectRandomPw = (int)(Math.random()*(passwordArray.length));
+            ranPw.append(passwordArray[selectRandomPw]);
+        }
+
+        return ranPw.toString();
     }
 
     @Override
--- src/main/java/com/jgji/spring/domain/user/service/UserService.java
+++ src/main/java/com/jgji/spring/domain/user/service/UserService.java
@@ -6,7 +6,6 @@ import com.jgji.spring.domain.user.domain.UserDTO.UserProfile;
 import com.jgji.spring.domain.user.repository.UserRepository;
 import com.jgji.spring.global.util.Utils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -28,6 +27,7 @@ public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bcryptPasswordEncoder;
+    private final UserSaveService userSaveService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -40,41 +40,9 @@ public class UserService implements UserDetailsService {
         AccountAdapter adapter = AccountAdapter.from(user);
         return adapter;
     }
-    
-    public User save(User user) {
-        user.update(bcryptPasswordEncoder.encode(user.getPassword()));
-        
-        return userRepository.save(user);
-    }
 
     public boolean isExistName(String userName) {
         return !ObjectUtils.isEmpty(userRepository.findByUsername(userName));
-    }
-    
-    public String setTempPassWord(User user) {
-        User updateUser = this.userRepository.findByUsername(user.getUsername());
-        String tempPassword = getTempPassword();
-        
-        updateUser.update(tempPassword);
-
-        return tempPassword;
-    }
-    
-    private String getTempPassword() {
-        char passwordArray[] = new char[] {
-                          '0','1','2','3','4','5','6','7','8','9',
-                          'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                          'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-                          '!','@','#','$','%','^','&','*','(',')'};
-
-        StringBuilder ranPw = new StringBuilder();
-
-        for (int i = 0; i < 16; i++) {
-          int selectRandomPw = (int)(Math.random()*(passwordArray.length));
-          ranPw.append(passwordArray[selectRandomPw]);
-        }
-        
-      return ranPw.toString();
     }
     
     public String changePassword(User user, UserProfile changePassword) {
@@ -84,9 +52,9 @@ public class UserService implements UserDetailsService {
             return validationmsg;
         }
 
-        user.update(changePassword.getNewPassword());
+        user.changePassword(changePassword.getNewPassword());
         
-        save(user);
+        this.userSaveService.save(user);
         
         return "성공";
     }
